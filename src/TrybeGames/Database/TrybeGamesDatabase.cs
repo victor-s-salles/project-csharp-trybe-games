a7859d63191d080ad0689f4faa3f452d76dad739
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-20 13:58:01 -0300

    Feat: Requisito 8

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -68,8 +68,12 @@ public class TrybeGamesDatabase
     // 8. Crie a funcionalidade de buscar todos os diferentes Tipos de jogos dentre os jogos cadastrados
     public List<GameType> GetGameTypes()
     {
-        // Implementar
-        throw new NotImplementedException();
+        var gameTypes = (
+            from game in Games
+            group game by game.GameType into gameType
+            select gameType.Key
+        ).ToList();
+        return gameTypes;
     }
 
     // 9. Crie a funcionalidade de buscar todos os estúdios de jogos junto dos seus jogos desenvolvidos com suas pessoas jogadoras
